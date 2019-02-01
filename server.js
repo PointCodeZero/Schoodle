9c7e8bd905ca69000c4f6e591288863889b363d5
vagrant <stan.solomean@gmail.com>
2019-02-01 16:38:26 +0000

    jquery slidedown on datetime; commmented out users routes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
-const usersRoutes = require("./routes/users");
+// const usersRoutes = require("./routes/users");
 const eventsRoutes = require("./routes/events");
 
 
@@ -43,7 +43,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+// app.use("/api/users", usersRoutes(knex));
 app.use("/events", eventsRoutes(knex));
 
 //Home page
